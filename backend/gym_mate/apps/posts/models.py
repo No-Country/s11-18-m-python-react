bcce3f063572d520243dbee8b94636baa1e4f1d9
JuanBianchi <juampibianchi065@gmail.com>
2023-10-10 17:52:19 -0300

    models created

--- backend/gym_mate/apps/posts/models.py
+++ backend/gym_mate/apps/posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
 # Create your models here.
+
+class Posts(TimeStampedModel):
+    content = models.CharField(max_length=256)
+    # user_id. Tomara el id_user del modelo User cuando este creado 
+
+class ImagePost(TimeStampedModel):
+    post_id =  models.ForeignKey(Posts, on_delete=models.CASCADE)
+    image_url = models.FileField(upload_to='posts/img',blank= True, null= True)
+
+class VideoPost(TimeStampedModel):
+    post_id =  models.ForeignKey(Posts, on_delete=models.CASCADE)
+    video_url = models.FileField(upload_to='posts/videos',blank= True, null= True)
+
+class CommentPost(TimeStampedModel):
+    content = models.CharField(max_length=256)
+    post_id =  models.ForeignKey(Posts, on_delete=models.CASCADE)
+    # user_id =
