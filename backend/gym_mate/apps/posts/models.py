b790f5961ef9c9404d496a19bf451cb054e7a056
LucasSimoesMDP <lucasgsimoes@icloud.com>
2023-10-12 16:52:43 -0300

    changed requirement/user model

--- backend/gym_mate/apps/posts/models.py
+++ backend/gym_mate/apps/posts/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
+from apps.users.models import User
 
 class Posts(TimeStampedModel):
     content = models.CharField(max_length=256)
-    # user_id. Tomara el id_user del modelo User cuando este creado 
-
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+     
 class ImagePost(TimeStampedModel):
     post_id =  models.ForeignKey(Posts, on_delete=models.CASCADE)
     image_url = models.FileField(upload_to='posts/img',blank= True, null= True)
@@ -16,14 +17,14 @@ class VideoPost(TimeStampedModel):
 class CommentPost(TimeStampedModel):
     content = models.CharField(max_length=256)
     post_id =  models.ForeignKey(Posts, on_delete=models.CASCADE)
-    # user_id =  Will take the id from the user who made the comment
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
 
 class Junction_likes(models.Model):
     post_id = models.ForeignKey(Posts, on_delete=models.CASCADE)
     total_likes = models.PositiveIntegerField(default = 0)
-    # user_id
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
 
 class Junction_repost(models.Model):
     post_id = models.ForeignKey(Posts, on_delete=models.CASCADE)
     total_repost = models.PositiveIntegerField(default = 0)
-    # user_id
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
