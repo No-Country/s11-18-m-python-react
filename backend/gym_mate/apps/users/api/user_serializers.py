7c43e4790b0319415f7935cc857acd075da6a95c
Leonel Rodriguez <114777251+Leonel0805@users.noreply.github.com>
2023-10-12 10:44:27 -0300

    merge users

--- backend/gym_mate/apps/users/api/user_serializers.py
+++ backend/gym_mate/apps/users/api/user_serializers.py
@@ -5,3 +5,9 @@ class UserTokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = User 
         fields = ('username', 'email')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("__all__")
