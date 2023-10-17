2a8588c4f4015fa398ab551f6951c7ccb6262af9
fabricio <fabricioreynacordes@gmail.com>
2023-10-17 18:05:15 -0300

    Coaches,Susbribite,Post

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,11 +1,12 @@
+import CoachesDest from '@/components/Coaches/Coaches'
 import Suscribite from '@/components/Suscribite/Suscribite'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   <h2 className='text-Turquesa/600 text-4xl'>aaa</h2>
+    <main>
     <Suscribite/>
+    <CoachesDest/>
     </main>
   )
 }
--- frontend/src/components/Suscribite/Suscribite.jsx
+++ frontend/src/components/Suscribite/Suscribite.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import CardMenAn from './cards/CardsMenAn'
 
 function Suscribite() {
   return (
-    <div>
-        
-        
+    <div className='  bg-Turqusa/100 w-fit  h-fit px-2 py-4'>
+        <h3 className='text-2xl text-Turquesa/600 font-semibold'>Suscribete</h3>
+        <span className='text-xl text-Blanco'>Disfruta los beneficios de ser premium</span>
+        <div className='grid grid-cols-2 gap-2 w-[90vh] mt-5  '> 
+        <CardMenAn/>
+
+        </div>
     </div>
   )
 }
