5f434e8a4658719204107d7473a68b51b8035fa7
FabricioReyna <fabricioreynacordes@gmail.com>
2023-10-17 10:55:09 -0300

    Components

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,9 +1,11 @@
+import Suscribite from '@/components/Suscribite/Suscribite'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
    <h2 className='text-Turquesa/600 text-4xl'>aaa</h2>
+    <Suscribite/>
     </main>
   )
 }
---
+++ frontend/src/components/Suscribite/Suscribite.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Suscribite() {
+  return (
+    <div>
+        
+        
+    </div>
+  )
+}
+
+export default Suscribite
