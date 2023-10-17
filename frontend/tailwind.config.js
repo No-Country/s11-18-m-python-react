daf42b12e21f61d8088b1b441b77127d3e8a39e2
FabricioReyna <fabricioreynacordes@gmail.com>
2023-10-17 10:51:42 -0300

    Colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,26 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        // Turquesa
+        "Turqusa/100" : "#031818",
+        "Turqusa/200" : "#1c3131",
+        "Turquesa/300" : "#386161",
+        "Turquesa/400" : "#539292",
+        "Turquesa/500" : "#6fc2c2",
+        "Turquesa/600" : "#8bf3f3",
+        "Turquesa/700" : "#b9f8f8",
+        "Turquesa/800" : "#b9f8f8",
+        "Turquesa/900" : "#d1fafa",
+        "Turquesa/1000" : "#e8fdfd",
+        // Grises
+
+        "Negro" : "#000606",
+        "Blanco" : "#FCFCFC",
+        "Grises/100" : "#535353",
+        "Grises/200" : "#a5a5a5",
+        "Grises/300" : "D8D8D8"
+       }
     },
   },
   plugins: [],
