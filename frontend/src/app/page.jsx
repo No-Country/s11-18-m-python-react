17802301d32366b6055903d90f29d50dd06acedd
FabricioReyna <fabricioreynacordes@gmail.com>
2023-10-20 10:43:31 -0300

    Changes

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,13 +1,10 @@
 import CoachesDest from '@/components/Coaches/Coaches'
 import PostPage from '@/components/Posts/post'
 import Suscribite from '@/components/Suscribite/Suscribite'
-<<<<<<< HEAD
-=======
 import Image from 'next/image'
 import MenuBar from '@/components/menuBar/MenuBar'
 import CarruselProfiles from '@/components/carruselProfiles/CarruselProfiles'
 import SearchContainer from '@/components/searchcontainer/SearchContainer'
->>>>>>> cb64a83a03df384ad859563280f9b0587e70a352
 
 export default function Home() {
   return (
@@ -15,8 +12,7 @@ export default function Home() {
       <MenuBar/>
       <CarruselProfiles/>
       <SearchContainer/>
-    
-    <Suscribite/>
+        <Suscribite/>
     <CoachesDest/>
     </main>
   )
