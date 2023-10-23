f5fedd602b2907923ccc52fcd2c00778f8de732f
Leonel Rodriguez <114777251+Leonel0805@users.noreply.github.com>
2023-10-23 10:09:47 -0300

    prod ready

--- backend/gym_mate/gym_mate/settings/prod.py
+++ backend/gym_mate/gym_mate/settings/prod.py
@@ -1,5 +1,5 @@
 from .base import *
-
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -22,11 +22,11 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = BASE_DIR.child('static')
-STATIC_ROOT = BASE_DIR.child('staticfiles')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.child('media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
