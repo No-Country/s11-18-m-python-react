a571afa9f3d7cc3b0f0582cf74bff643e1130c6e
rodrjm <rodrjm94@gmail.com>
2023-10-24 11:02:58 -0300

    Modificaciones en las views de coach
    Optimizaciones para verificaciones de logueo

--- backend/gym_mate/apps/coach_users/api/urls.py
+++ backend/gym_mate/apps/coach_users/api/urls.py
@@ -1,24 +1,25 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     # Rating Coach
     # Endpoint para obtener calificaciones de un coach
     path('ratings/coach/<int:coach_id>/', views.get_ratings_for_coach, name='get_ratings_for_coach'),
-    # Endpoint para crear una calificación
-    path('ratings/create/', views.create_rating, name='create_rating'),
-    # Endpoint para actualizar una calificación
-    path('ratings/update/<int:rating_id>/', views.update_rating, name='update_rating'),
+    # Endpoint para crear una calificación de un coach
+    path('ratings/create/<int:coach_id>/', views.create_rating, name='create_rating'),
+    # Endpoint para actualizar una calificación de un coach
+    path('ratings/update/<int:coach_id>/<int:rating_id>/', views.update_rating, name='update_rating'),
     # Endpoint para obtener el promedio de valoraciones de un coach
-    path('ratings/average/coach/<int:coach_id>/', views.get_average_rating_for_coach, name='get_average_rating_for_coach'),
+    #path('ratings/average/coach/<int:coach_id>/', views.get_average_rating_for_coach, name='get_average_rating_for_coach'),
 
     #Verification Coach
     # Endpoint para crear una verificación de coach
     path('verifications/create/', views.create_verification, name='create_verification'),
-    # Endpoint para listar las verificaciones de coach del usuario actual
-    path('verifications/', views.list_verifications, name='list_verifications'),
+    # Endpoint para listar las verificaciones para un coach específico
+    path('verifications/<int:coach_id>/', views.list_verifications, name='list_verifications'),
     # Endpoint para obtener detalles de una verificación de coach específica
-    path('verifications/<int:verification_id>/', views.get_verification, name='get_verification'),
+    path('verification/<int:verification_id>/', views.get_verification, name='get_verification'),
     # Endpoint para actualizar una verificación de coach
     path('verifications/<int:verification_id>/update/', views.update_verification, name='update_verification'),
     # Endpoint para eliminar una verificación de coach
@@ -27,8 +28,6 @@ urlpatterns = [
     #Gym Subscription
     # Endpoint para crear una suscripción a gimnasio
     path('subscriptions/create/', views.create_subscription, name='create_subscription'),
-    # Endpoint para listar las suscripciones a gimnasios del usuario actual
-    # path('subscriptions/', views.list_subscriptions, name='list_subscriptions'),
     # Endpoint para obtener detalles de una suscripción a gimnasio específica
     path('subscriptions/<int:subscription_id>/', views.get_subscription, name='get_subscription'),
     # Endpoint para actualizar una suscripción a gimnasio
--- backend/gym_mate/apps/coach_users/models.py
+++ backend/gym_mate/apps/coach_users/models.py
@@ -13,6 +13,7 @@ class Rating_Coach(models.Model):
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='ratings_given')
     rated_coach = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='ratings_received')
 
+
 class Verification_Coach(models.Model):
     degree = models.CharField(max_length=50)
     academic_institution = models.CharField()
