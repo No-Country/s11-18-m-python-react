f19967f8dee07909efa9457c11d8a42a414c9c69
juanpablo1978 <jpandrujovich@gmail.com>
2023-10-29 22:25:37 -0300

    crear ejercicios

--- frontend/src/app/crearEjercicios/page.jsx
+++ frontend/src/app/crearEjercicios/page.jsx
@@ -17,16 +17,17 @@ const CrearEjercicios = () => {
             <div className='w-[44px] h-[8px] rounded-l-[8px] bg-Turquesa/600'>
             </div>
             
-                <div className='rounded-full border-Turquesa/600 border-[1px]
-                 bg-Turqusa/20 flex items-center justify-center w-[32px] h-[32px] bg-Turqusa/200'>
+                <div className='rounded-full 
+                  flex items-center justify-center w-[32px] h-[32px] bg-Turqusa/200'>
                     1
                 </div>
                 
-            <div className='w-[90px] h-[8px] border-Turquesa/600 border-t-[1px] border-b-[1px]'>
+            <div className='w-[90px] h-[8px] bg-Turquesa/600 border-t-[1px] border-b-[1px]'>
             </div>
             
                 <div className='rounded-full
-                 bg-Turqusa/200 w-[32px] h-[32px] flex items-center justify-center'>
+                 bg-Turqusa/200 w-[32px] h-[32px] flex items-center justify-center
+                 border-Turquesa/600 border-[1px]'>
                     2
                 </div>
                
@@ -58,26 +59,32 @@ const CrearEjercicios = () => {
     
     <div className='flex justify-center'>
        <article>
-        <h3 className='text-[24px] font-normal text-Turquesa/600 py-4'>
-            Crea tu primera rutina
+        <h3 className='text-[24px] font-normal text-Turquesa/600 py-5'>
+            Ejercicios
             </h3>
 
-            <p className='text-white'>Completá con los datos que tendrá tu rutina. </p>
+            <p className='text-white'>
+            Ya creaste tu rutina, ahora podés agregar sets
+            <br />
+             con los ejercicios y detalles correspondientes
+             <br />
+              para cada una.
+            </p>
 
      
-    <div className='flex justify-center gap-4 pt-[56px]'>
+    <div className='flex justify-center gap-4 pt-[62px]'>
     <button className='w-[182px] h-[40px] bg-transparent text-Turquesa/600 rounded-[8px] font-normal
      border-Turquesa/600 border-[1px] text-[18px]'>
-        Salir
+        Volver
     </button>
     <button className='w-[182px] h-[40px] bg-Turquesa/900 rounded-[8px] font-normal
      text-gray-700 text-[18px'>
-        Comenzar
+        Agregar set
     </button>
 </div>
 
 
-<div className='flex justify-center items-end mt-[140px]'>
+<div className='flex justify-center items-end mt-[310px]'>
 <div className='border-[2px] border-white w-[105px]'></div>
 </div>
 
