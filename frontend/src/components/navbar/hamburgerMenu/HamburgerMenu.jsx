0b348796e65776f04e4741070b22a023ee1eacf9
FabricioReyna <fabricioreynacordes@gmail.com>
2023-10-31 11:26:53 -0300

    FrontEnd

--- frontend/src/components/navbar/hamburgerMenu/HamburgerMenu.jsx
+++ frontend/src/components/navbar/hamburgerMenu/HamburgerMenu.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import { AiOutlineClose } from "react-icons/ai";
 import CloseButton from './CloseButton';
+import Link from 'next/link';
 
 
 const HamburgerMenu = ({toggleOpen}) => {
@@ -15,11 +16,41 @@ const HamburgerMenu = ({toggleOpen}) => {
     <article className={  CloseView ?  `bg-Turquesa/300 w-[275px] h-[733px] rounded-r-[8px] absolute bottom-[-26.5em] z-10 left-[-1.9em]` : `hidden`}>
   <CloseButton  ButtonClose={handleClose}  />        
   <ul className='text-white leading-[57px] pl-[28px] pt-[62px] text-xl font-light'>
-            <li>Feed</li>
-            <li>Rutinas</li>
-            <li>Coach</li>
-            <li>Mi perfil</li>
-            <li>Ayuda</li>
+            <li>
+              <Link href={"/a/feed"}>
+              
+              Feed
+              </Link> 
+              
+              </li>
+            <li>
+              <Link href={"/a/rutinas"}>
+              
+              Rutinas
+              </Link> 
+              
+              </li>
+            <li>
+              <Link href={"/a/coach"}>
+              
+              Coach
+              </Link> 
+              
+              </li>
+            <li>
+              <Link href={"/a/perfil"}>
+              
+              Mi perfil
+              </Link> 
+              
+              </li>
+            <li>
+              <Link href={"/a/ayuda"}>
+              
+              Ayuda
+              </Link> 
+              
+              </li>
         </ul>
     </article>
   )
