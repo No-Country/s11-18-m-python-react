bb84b14ee54cdf2cfe4bb37ed4a18f96c3c2b27e
FabricioReyna <fabricioreynacordes@gmail.com>
2023-11-02 03:52:43 -0300

    Frontend

--- frontend/src/app/a/cargaFoto/page.jsx
+++ frontend/src/app/a/cargaFoto/page.jsx
@@ -1,29 +1,29 @@
-import CrearPost from '@/components/crearPost/CrearPost'
-import PublicacionTexto from '@/components/crearPost/PublicacionTexto'
-import RutinaPaga from '@/components/crearPost/RutinaPaga'
+// import CrearPost from '@/components/crearPost/CrearPost'
+// import PublicacionTexto from '@/components/crearPost/PublicacionTexto'
+// import RutinaPaga from '@/components/crearPost/RutinaPaga'
 
-import React from 'react'
+// import React from 'react'
 
-const CargaFoto = () => {
-  return (
+// const CargaFoto = () => {
+//   return (
     
-    <main className='w-[412px]  min-h-screen bg-black flex flex-col items-center'>
+//     <main className='w-[412px]  min-h-screen bg-black flex flex-col items-center'>
 
-        <div className='pb-6'>
+//         <div className='pb-6'>
     
-        </div>
-        <div className='flex flex-col gap-12'>
-        <CrearPost/>
-        <RutinaPaga/>
-        <PublicacionTexto/>
-        </div>
-        <div className='flex justify-center items-end mt-[90px] mb-5'>
-    <div className='border-[2px] border-white w-[105px]'></div>
-    </div>
+//         </div>
+//         <div className='flex flex-col gap-12'>
+//         <CrearPost/>
+//         <RutinaPaga/>
+//         <PublicacionTexto/>
+//         </div>
+//         <div className='flex justify-center items-end mt-[90px] mb-5'>
+//     <div className='border-[2px] border-white w-[105px]'></div>
+//     </div>
 
-    </main>
+//     </main>
     
-  )
-}
+//   )
+// }
 
-export default CargaFoto
+// export default CargaFoto
